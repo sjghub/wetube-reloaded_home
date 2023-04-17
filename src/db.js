08ebae305129140efabcd5049f90033be06c1380
esfuerzo <dkdldrha@naver.com>
2023-04-17 23:43:51 +0900

    Connecting mongo and create schema

--- src/db.js
+++ src/db.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 mongoose.connect("mongodb://127.0.0.1:27017/wetube");
 
 const db = mongoose.connection;
-const hadleOpen = () => console.log("Connected to DB");
-const hadleError = (error) => console.log("DB Error", error);
+const hadleOpen = () => console.log("✅Connected to DB");
+const hadleError = (error) => console.log("❌DB Error", error);
 db.on("error", hadleError);
 db.once("open", hadleOpen);
