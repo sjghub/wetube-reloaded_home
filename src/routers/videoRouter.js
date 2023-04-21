454098683699b73011a5c3404cfccac02044e694
esfuerzo <dkdldrha@naver.com>
2023-04-21 13:13:24 +0900

    Delete Videos

--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -5,11 +5,13 @@ import {
   postEdit,
   getUpload,
   postUpload,
+  deleteVideo,
 } from "../controllers/videocontrollers";
 
 const videoRouter = express.Router();
 
 videoRouter.get("/:id([0-9a-f]{24})", watch);
+videoRouter.get("/:id([0-9a-f]{24})/delete", deleteVideo);
 videoRouter.route("/:id([0-9a-f]{24})/edit").get(getEdit).post(postEdit);
 videoRouter.route("/upload").get(getUpload).post(postUpload);
 
