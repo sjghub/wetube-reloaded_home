1b139bb9bb84a245f96e107b8bc1ca5db8645121
esfuerzo <dkdldrha@naver.com>
2023-04-26 06:14:08 +0900

    Form Validation

--- src/controllers/userControllers.js
+++ src/controllers/userControllers.js
@@ -1,15 +1,30 @@
 import User from "../models/User";
 
 export const getJoin = (req, res) => {
-  res.render("join", { pagetitle: "Join" });
+  return res.render("join", { pagetitle: "Join" });
 };
 export const postJoin = async (req, res) => {
-  const { name, email, username, password, location } = req.body;
+  const { name, email, username, password1, password2, location } = req.body;
+  if (password1 !== password2) {
+    return res.render("join", {
+      pagetitle: "Join",
+      errormessage: "Password confirmation does not match",
+    });
+  }
+  const exists = await User.exists({ $or: [{ username }, { email }] });
+  if (exists) {
+    return res.render("join", {
+      pagetitle: "Join",
+      errormessage: "username or email already exists",
+    });
+  }
+
   await User.create({
     name,
     email,
     username,
-    password,
+    password1,
+    password2,
     location,
   });
   return res.redirect("/login");
