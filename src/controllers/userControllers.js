2ceb40befb13788f0b578a265355b04d3d4baacb
esfuerzo <dkdldrha@naver.com>
2023-04-26 05:24:25 +0900

    Creating Account page

--- src/controllers/userControllers.js
+++ src/controllers/userControllers.js
@@ -1,4 +1,20 @@
-export const join = (req, res) => res.send("Join");
+import User from "../models/User";
+
+export const getJoin = (req, res) => {
+  res.render("join", { pagetitle: "Join" });
+};
+export const postJoin = async (req, res) => {
+  const { name, email, username, password, location } = req.body;
+  await User.create({
+    name,
+    email,
+    username,
+    password,
+    location,
+  });
+  console.log(req.body);
+  return res.redirect("/login");
+};
 export const deleteUser = (req, res) => res.send("Delete users");
 export const edit = (req, res) => res.send("Edit users");
 export const login = (req, res) => res.send("Login");
--- src/db.js
+++ src/db.js
@@ -4,6 +4,7 @@ mongoose.connect("mongodb://127.0.0.1:27017/wetube", {
   useUnifiedTopology: true,
   useNewUrlParser: true,
   useFindAndModify: false,
+  useCreateIndex: true,
 });
 
 const db = mongoose.connection;
