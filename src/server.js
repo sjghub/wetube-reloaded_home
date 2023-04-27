b828baabe5111f5eaca5b8e3c1f21d5f38a2556d
esfuerzo <dkdldrha@naver.com>
2023-04-28 00:04:02 +0900

    MongoDB, env

--- src/server.js
+++ src/server.js
@@ -1,10 +1,11 @@
 import express from "express";
 import morgan from "morgan";
 import session from "express-session";
-import { localsMiddleware } from "./middlewares";
+import MongoStore from "connect-mongo";
 import rootRouter from "./routers/rootRouter";
 import videoRouter from "./routers/videoRouter";
 import userRouter from "./routers/userRouter";
+import { localsMiddleware } from "./middlewares";
 
 const app = express();
 
@@ -17,9 +18,10 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(
   session({
-    secret: "Hello",
-    resave: true,
-    saveUninitialized: true,
+    secret: process.env.COOKIE_SECRET,
+    resave: false,
+    saveUninitialized: false,
+    store: MongoStore.create({ mongoUrl: process.env.DB_URL }),
   })
 );
 
