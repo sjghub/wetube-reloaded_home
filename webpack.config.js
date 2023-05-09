d1debed6019b07255bcf8a21a9470c3fce5a3fa9
esfuerzo <dkdldrha@naver.com>
2023-05-10 02:58:16 +0900

    Video player : play,pause,mute,volume

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,10 @@ const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const path = require("path");
 module.exports = {
   mode: "development",
-  entry: "./src/client/js/main.js",
+  entry: {
+    main: "./src/client/js/main.js",
+    videoPlayer: "./src/client/js/videoPlayer.js",
+  },
   watch: true,
   plugins: [
     new MiniCssExtractPlugin({
@@ -11,7 +14,7 @@ module.exports = {
     }),
   ],
   output: {
-    filename: "js/main.js",
+    filename: "js/[name].js",
     path: path.resolve(__dirname, "assets"),
   },
   module: {
