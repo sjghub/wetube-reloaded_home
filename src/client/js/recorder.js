8d5054196c4b9f1d50d369ceb461f699a93e6239
esfuerzo <dkdldrha@naver.com>
2023-05-11 14:21:58 +0900

    recording and donwloading video

--- src/client/js/recorder.js
+++ src/client/js/recorder.js
@@ -1,13 +1,45 @@
 const startBtn = document.getElementById("startBtn");
 const video = document.getElementById("preview");
-const handleStart = async () => {
-  const stream = await navigator.mediaDevices.getUserMedia({
+
+let stream;
+let recorder;
+let videoFile;
+
+const handleDownload = () => {
+  const a = document.createElement("a");
+  a.href = videoFile;
+  a.download = "MyRecording.webm";
+  document.body.appendChild(a);
+  a.click();
+};
+
+const handleStop = () => {
+  startBtn.innerText = "Download Recording";
+  startBtn.removeEventListener("click", handleStop);
+  startBtn.addEventListener("click", handleDownload);
+  recorder.stop();
+};
+const handleStart = () => {
+  startBtn.innerText = "Stop Recording";
+  startBtn.removeEventListener("click", handleStart);
+  startBtn.addEventListener("click", handleStop);
+  recorder = new MediaRecorder(stream);
+  recorder.ondataavailable = (e) => {
+    videoFile = URL.createObjectURL(e.data);
+    video.srcObject = null;
+    video.src = videoFile;
+    video.loop = true;
+    video.play();
+  };
+  recorder.start();
+};
+const init = async () => {
+  stream = await navigator.mediaDevices.getUserMedia({
     audio: false,
     video: true,
   });
   video.srcObject = stream;
   video.play();
-  console.log(stream);
 };
-
+init();
 startBtn.addEventListener("click", handleStart);
